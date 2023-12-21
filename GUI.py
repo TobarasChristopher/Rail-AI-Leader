38bcee83fd61bd5b650bd2ab32fa153f3c116961
unknown <tobaraschristopher@yahoo.ie>
2023-12-21 12:26:07 +0000

    add GUI, buttons and window attributes

--- GUI.py
+++ GUI.py
@@ -2,9 +2,39 @@ import tkinter as tk
 
 window = tk.Tk()
 
-window.title("Example Test")
+window.title("Rail AI Leader")
 
-label = tk.Label(window, text="Hello, Tkinter!")
-label.pack()
+#Frames
+
+label = tk.Label(window, text="Version 0.1 Prototype")
+label.pack(anchor="nw", padx=5)
+
+buttonFrame = tk.Frame(window)
+buttonFrame.pack(anchor="nw", padx=5)
+
+#Buttons
+
+startButton = tk.Button(buttonFrame, text ="Start", width=15, height=7, bg="#3bbd24", fg="black")
+startButton.grid(row=0,column=1, padx=5)
+
+stopButton = tk.Button(buttonFrame, text ="Stop", width=15, height=7, bg="#f02c1a", fg="black")
+stopButton.grid(row=0,column=2, padx=5)
+
+feedButton = tk.Button(buttonFrame, text ="Feed Schedule", width=15, height=7, bg="#3882c7", fg="black")
+feedButton.grid(row=0,column=3, padx=5)
+
+logsButton = tk.Button(buttonFrame, text ="View Logs", width=15, height=7, bg="#dbdb16", fg="black")
+logsButton.grid(row=0,column=4, padx=5)
+
+debugButton = tk.Button(buttonFrame, text ="Debug", width=15, height=7, bg="#ed951a", fg="black")
+debugButton.grid(row=0,column=5, padx=5)
+
+manualButton = tk.Button(buttonFrame, text ="Manual", width=15, height=7, bg="#8137a1", fg="black")
+manualButton.grid(row=0,column=6, padx=5)
+
+#Window Attributes
+
+window.geometry("775x160")
+window.resizable(False, False)
 
 window.mainloop()
---
+++ init.py
@@ -0,0 +1,4 @@
+import GUI
+import ProtoEnviro as Proto
+
+GUI()
